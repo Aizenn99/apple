29a4190388a1f734700d96057fcbcf65d3f612a2
het <kalriyahet@gmail.com>
2024-10-30 00:45:27 +0530

    video is done all video works now

--- src/components/VideoCards.jsx
+++ src/components/VideoCards.jsx
@@ -18,8 +18,15 @@ const VideoCards = () => {
     isPlaying: false,
   });
   const { isEnd, startplay, isLastVideo, isPlaying, videoId } = video;
+ 
 
   useGSAP(() => {
+    gsap.to("#slider", {
+    transform:`translateX(${-100 * videoId }%)`,
+    duration:2,
+    ease:'power2.inOut'
+      
+    });
     gsap.to("#video", {
       scrollTrigger: {
         trigger: "#video",
@@ -45,28 +52,56 @@ const VideoCards = () => {
     }
   }, [startplay, isPlaying, videoId, LoadedData]);
 
-  const handelLoadedMetadata = (i,e) => setLoadedData((prevVideo) => [...prevVideo,e] )
+  const handelLoadedMetadata = (i, e) =>
+    setLoadedData((prevVideo) => [...prevVideo, e]);
 
   useEffect(() => {
     let currentProgress = 0;
     let span = videoSpanRef.current;
     if (span[videoId]) {
       let anim = gsap.to(span[videoId], {
-        onUpdate: ( ) => {
+        onUpdate: () => {
           const progress = Math.ceil(anim.progress() * 100);
-          if(progress != currentProgress ){
+          if (progress != currentProgress) {
             currentProgress = progress;
-            gsap.to(videoDivREf.current[videoId],{
-              width:window.innerWidth < 760 ? '10vw' : window.innerWidth < 1200 ? '10vw' : '4vw'
-            })
-            gsap.to(span[videoId],{
-              width: `${currentProgress}%` ,
-              backgroundColor:'white',
-            })
+            gsap.to(videoDivREf.current[videoId], {
+              width:
+                window.innerWidth < 760
+                  ? "10vw"
+                  : window.innerWidth < 1200
+                  ? "10vw"
+                  : "4vw",
+            });
+            gsap.to(span[videoId], {
+              width: `${currentProgress}%`,
+              backgroundColor: "white",
+            });
           }
         },
-        onComplete: () => {},
+        onComplete: () => {
+          if (isPlaying) {
+            gsap.to(videoDivREf.current[videoId], {
+              width: "12px",
+            });
+            gsap.to(span[videoId], {
+              backgroundColor: "#afafaf",
+            });
+          }
+        },
       });
+      if (videoId == 0) {
+        anim.restart();
+      }
+      const animUpdate = () => {
+        anim.progress(
+          videoRef.current[videoId].currentTime / hightlightsSlides[videoId].videoDuration
+        );
+      };
+      if (isPlaying) {
+        gsap.ticker.add(animUpdate);
+      } else {
+        gsap.ticker, remove(animUpdate);
+      }
     }
   }, [videoId, startplay]);
 
@@ -92,14 +127,23 @@ const VideoCards = () => {
           isLastVideo: false,
           videoId: 0,
         }));
-      case "video-play":
+        break;
+      
+      case "play":
         setVideo((prevVideo) => ({
           ...prevVideo,
-          isPlaying: !isPlaying,
+          isPlaying: !prevVideo.isPlaying,
         }));
 
         break;
+      case "pause":
+        setVideo((prevVideo) => ({
+          ...prevVideo,
+          isPlaying: !prevVideo.isPlaying,
+        }));
 
+        break;
+ 
       default:
         return video;
     }
@@ -117,15 +161,23 @@ const VideoCards = () => {
                   playsInline={true}
                   preload="auto"
                   muted
+                  className={
+                    `${list.id === 2 && 'translate-x-44'}
+                   pointer-events-none
+                  `}
                   ref={(el) => (videoRef.current[i] = el)}
+                  onEnded={() => {
+                    i !== 3
+                      ? handelProcess("video-end", i)
+                      : handelProcess("video-last");
+                  }}
                   onPlay={() => {
                     setVideo((prevVideo) => ({
                       ...prevVideo,
                       isPlaying: true,
                     }));
                   }}
-
-                  onLoadedMetadata={(e) => handelLoadedMetadata(i,e ) }
+                  onLoadedMetadata={(e) => handelLoadedMetadata(i, e)}
                 >
                   <source src={list.video} type="video/mp4" />
                 </video>
@@ -148,7 +200,6 @@ const VideoCards = () => {
               key={i}
               ref={(el) => (videoDivREf.current[i] = el)}
               className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
-            
             >
               <span
                 className="absolute rounded-full h-full w-full"
